fb84d28bd9d837e44c5285b09247f625e3aa9a48
mylesroot <mylesnichroot@gmail.com>
2025-03-09 23:25:27 +0000

    fix vercel

--- middleware.ts
+++ middleware.ts
@@ -24,5 +24,7 @@ export default clerkMiddleware(async (auth, req) => {
 })
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"]
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|manifest\\.json$).*)"
+  ]
 }
